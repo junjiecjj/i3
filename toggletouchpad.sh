9a65fd9117c2ecd73371934e363251a77730ac55
dell-xps <2716705056@qq.com>
2022-05-11 18:50:04 +0800

    just update

--- toggletouchpad.sh
+++ toggletouchpad.sh
@@ -1,15 +1,16 @@
 #! /usr/bin/env bash
 #########################################################################
-# File Name: wallpaper.sh
+# File Name: toggletouchpad.sh
 # Author:陈俊杰
 # mail: 2716705056@qq.com
-# Created Time: 2022年05月11日 星期三 18时38分41秒
+# Created Time: 2022年05月11日 星期三 15时34分40秒
 
 # 此程序的功能是：
 #########################################################################
 
 
-while true; do
-	feh --recursive --randomize --bg-fill ~/图片/Wallpapers/
-	sleep 30m
-done
+if synclient -l | grep "TouchpadOff .*=.*0" ; then
+    synclient TouchpadOff=1 ;
+else
+    synclient TouchpadOff=0 ;
+fi
